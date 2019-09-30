dc47791fde4faa32da91790e4f95341ca0eef581
Linlin Yan <yanlinlin82@gmail.com>
2019-09-30 21:54:18 +0800

    Improve performance

---
+++ 00001_two-sum/190930-3.cpp
@@ -0,0 +1,30 @@
+// https://leetcode-cn.com/problems/two-sum/
+#include <cstdio>
+#include <vector>
+#include <map>
+using namespace std;
+
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		map<int, int> hash;
+		for (int i = 0; i < nums.size(); ++i) {
+			auto it = hash.find(nums[i]);
+			if (it != hash.end()) {
+				return vector<int>{it->second, i};
+			}
+			hash[target - nums[i]] = i;
+		}
+		return vector<int>();
+	}
+};
+
+int main()
+{
+	vector<int> nums = {2, 7, 11, 15}; // answer: [0, 1]
+	int target = 9;
+	Solution s;
+	vector<int> r = s.twoSum(nums, target);
+	printf("[%d, %d]\n", r[0], r[1]);
+	return 0;
+}
