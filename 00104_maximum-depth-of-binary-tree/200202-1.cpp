ca487abb67c9c4c977b20165f5bd5ebb540b3312
Linlin Yan <yanlinlin82@gmail.com>
2020-02-02 16:50:32 +0800

    solve 104

---
+++ 00104_maximum-depth-of-binary-tree/200202-1.cpp
@@ -0,0 +1,94 @@
+// maximum-depth-of-binary-tree
+#include <cstdio>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+TreeNode* create(initializer_list<int> a)
+{
+	if (a.size() == 0) return NULL;
+	auto it = a.begin();
+	if (*it <= 0) return NULL;
+	TreeNode* t = new TreeNode(*it);
+	vector<TreeNode*> parents{t};
+	vector<TreeNode*> children;
+	++it;
+	for (size_t i = 0; it != a.end();) {
+		if (i < parents.size() * 2) {
+			TreeNode* c = (*it <= 0 ? NULL : new TreeNode(*it));
+			TreeNode* n = parents[i / 2];
+			if (i % 2 == 0) { n->left = c; } else { n->right = c; }
+			if (c) children.push_back(c);
+			++i; ++it;
+		} else {
+			parents = children;
+			children.clear();
+			i = 0;
+		}
+	}
+	return t;
+}
+
+void release(TreeNode* t)
+{
+	if (t) {
+		release(t->left);
+		release(t->right);
+		delete t;
+	}
+}
+
+void print(TreeNode* t)
+{
+	vector<TreeNode*> a{t};
+	int last = 0;
+	for (int i = 0;;) {
+		int end = a.size();
+		for (; i < end; ++i) {
+			a.push_back(a[i] ? a[i]->left : NULL);
+			a.push_back(a[i] ? a[i]->right : NULL);
+			if (a[i]) {
+				if (a[i]->right) last = a.size() - 1;
+				else if (a[i]->left) last = a.size() - 2;
+			}
+		}
+		if (last < end) break;
+	}
+	a.resize(last + 1);
+	printf("[ ");
+	for (auto e : a) {
+		if (e) {
+			printf("%d ", e->val);
+		} else {
+			printf("null ");
+		}
+	}
+	printf("]\n");
+}
+
+class Solution {
+public:
+	int maxDepth(TreeNode* root) {
+		if (!root) return 0;
+		int a = maxDepth(root->left);
+		int b = maxDepth(root->right);
+		return (a >= b ? a : b) + 1;
+	}
+};
+
+int main()
+{
+	Solution s;
+	{
+		TreeNode* t = create({3,9,20,NULL,NULL,15,7});
+		printf("%d\n", s.maxDepth(t));
+		release(t);
+	}
+	return 0;
+}
