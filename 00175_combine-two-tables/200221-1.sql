557abbab4943f6d30f91e121a9ba7d4f270ab73e
Linlin Yan <yanlinlin82@gmail.com>
2020-02-24 02:52:27 +0800

    Update 175 & 176

--- 00175_combine-two-tables/200221-1.sql
+++ 00175_combine-two-tables/200221-1.sql
@@ -1,3 +1,15 @@
 # https://leetcode-cn.com/problems/combine-two-tables/
+
+# SQL schema
+Create table Person (PersonId int, FirstName varchar(255), LastName varchar(255));
+Create table Address (AddressId int, PersonId int, City varchar(255), State varchar(255));
+Truncate table Person;
+insert into Person (PersonId, LastName, FirstName) values ('1', 'Wang', 'Allen');
+Truncate table Address;
+insert into Address (AddressId, PersonId, City, State) values ('1', '2', 'New York City', 'New York');
+
 # Write your MySQL query statement below
-select FirstName, LastName, City, State from Person left join Address using (PersonId);
+SELECT FirstName, LastName, City, State
+FROM Person
+LEFT JOIN Address
+USING (PersonId);
--- 00176_second-highest-salary/200221-1.sql
+++ 00176_second-highest-salary/200221-1.sql
@@ -1,12 +1,20 @@
 # https://leetcode-cn.com/problems/second-highest-salary/
+
+# SQL schema
+Create table If Not Exists Employee (Id int, Salary int);
+Truncate table Employee;
+insert into Employee (Id, Salary) values ('1', '100');
+insert into Employee (Id, Salary) values ('2', '200');
+insert into Employee (Id, Salary) values ('3', '300');
+
 # Write your MySQL query statement below
-select SecondHighestSalary
-from (select 2 as id) as a
-left join (
-    select
-        @i := @i + 1 as id,
-        t.*
-    from
-        (select distinct(Salary) as SecondHighestSalary from Employee order by Salary desc limit 2) as t,
-        (select @i := 0) as foo
-) as b on a.id = b.id;
+SELECT SecondHighestSalary
+FROM (SELECT 2 AS id) AS a
+LEFT JOIN (
+  SELECT
+    @i := @i + 1 AS id,
+    t.*
+  FROM
+    (SELECT DISTINCT(Salary) AS SecondHighestSalary FROM Employee ORDER BY Salary DESC limit 2) AS t,
+    (SELECT @i := 0) AS foo
+) AS b ON a.id = b.id;
